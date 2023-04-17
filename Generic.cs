9188879f00232b073228776404ce79d320c41e05
gaurangpatel406 <gaurangpatel406@yahoo.ca>
2023-04-17 19:54:47 -0400

    Add project files.

---
+++ Generic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Generic<T> where T : struct
+{
+    private List<T> values;
+
+    public Generic()
+    {
+        values = new List<T>();
+    }
+
+    public void Add(T item)
+    {
+        values.Add(item);
+    }
+
+    public T Get(int index)
+    {
+        if (index >= values.Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return values[index];
+    }
+
+    public List<T> GetDescending()
+    {
+        List<T> descendingList = values.OrderByDescending(x => x).ToList();
+        return descendingList;
+    }
+}
